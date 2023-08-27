2ca35c8eb50def376977543db9b39489e7819f2c
Sebastiao Matusse <matussevalete@gmail.com>
2023-08-27 15:45:20 -0600

    added the map in the report page

---
+++ script/maps.js
@@ -0,0 +1,36 @@
+let map = L.map("map").setView([-19.81981378095318, 34.8617618481015], 14);
+let geoloc = document.querySelector("#geoloc");
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
+
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+let circle = L.circle([-19.81981378095318, 34.8617618481015], {
+  color: "green",
+  fillColor: "#ff7800",
+  fillOpacity: 0.3,
+  radius: 1000,
+  weight: 0.3,
+}).addTo(map);
+
+let popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+
+  geoloc.value = e.latlng.toString();
+}
+
+map.on("click", onMapClick);
+
+let polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047],
+]).addTo(map);
